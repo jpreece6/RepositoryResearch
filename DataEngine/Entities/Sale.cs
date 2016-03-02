3925396ceaaa4e11bf5c669697b029a25ff547d7
Josh Preece <joshpreece@outlook.com>
2016-03-02 14:30:02 +0000

    Added sales entity

---
+++ DataEngine/Entities/Sale.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataEngine.Entities
+{
+    public class Sale : IEntity
+    {
+        public virtual int Id { get; protected set; }
+        public virtual int StoreId { get; set; }
+        public virtual int ProductId { get; set; }
+        public virtual DateTime Timestamp { get; set; }
+    }
+}
---
+++ DataEngine/Mapping/SaleMap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataEngine.Entities;
+using FluentNHibernate.Mapping;
+
+namespace DataEngine.Mapping
+{
+    public class SaleMap : ClassMap<Sale>
+    {
+        public SaleMap()
+        {
+            Id(x => x.Id);
+            Map(x => x.StoreId);
+            Map(x => x.ProductId);
+            Map(x => x.Timestamp);
+        }
+    }
+}
