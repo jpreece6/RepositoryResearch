5c3bf2c35622cd436f0d3af6623814016a5a4c23
Josh Preece <joshpreece@outlook.com>
2016-03-07 18:26:07 +0000

    Added sync to start and exit

--- RepoConsole/Program.cs
+++ RepoConsole/Program.cs
@@ -1,10 +1,12 @@
 ﻿ using System;
 using System.Collections.Generic;
-using System.Linq;
+ using System.Data;
+ using System.Linq;
  using System.Runtime.InteropServices;
  using System.Text;
 using System.Threading.Tasks;
-using DataEngine;
+ using System.Xml.Linq;
+ using DataEngine;
 using DataEngine.Contexts;
 using DataEngine.Entities;
  using Helpers;
@@ -15,76 +17,61 @@ namespace RepoConsole
 {
     class Program
     {
+        private static SyncManager _sync;
+        private static ConfigReader _configReader;
+
         static void Main(string[] args)
         {
             // Load settings
-            var configReader = new ConfigReader(@"C:\repoSettings.xml");
-            BaseConfig.Sources = configReader.GetAllInstancesOf("ConnectionString");
+            _configReader = new ConfigReader(@"C:\repoSettings.xml");
+            BaseConfig.Sources = _configReader.GetAllInstancesOf("ConnectionString");
 
-            var sync = new SyncManager();
+            // Register sync events
+            _sync = new SyncManager();
+            _sync.OnSyncComplete += Sync_OnSyncComplete;
+            _sync.OnSyncFailure += Sync_OnSyncFailure;
+            _sync.OnUpdateStatus += Sync_OnUpdateStatus;
+            _sync.OnSyncStart += Sync_OnSyncStart;
+            _sync.OnCleanUp += Sync_OnCleanUp;
 
-            sync.OnSyncComplete += Sync_OnSyncComplete;
-            sync.OnSyncFailure += Sync_OnSyncFailure;
-            sync.OnUpdateStatus += Sync_OnUpdateStatus;
-            sync.OnSyncStart += Sync_OnSyncStart;
-            sync.OnCleanUp += Sync_OnCleanUp;
-
-            sync.SyncAllTables();
+            Sync();
+            BaseConfig.IsLocal = false;
 
             var main = new MainView();
             main.Show();
 
-            sync.SyncAllTables();
+            BaseConfig.IsLocal = false;
+            Sync();
 
-            /*
-            // Load settings
-            var configReader = new ConfigReader(@"C:\repoSettings.xml");
-            BaseConfig.Sources = configReader.GetAllInstancesOf("ConnectionString");
-
-            // Init page.. 
-            var sessionFactManager = new SessionFactoryManager();
-            var sessionContext = new SessionContext(sessionFactManager);
-            sessionContext.OpenContextSession();
-
-            var index = 0;
-
-
-            EmployeeRepository<Employee> repo = new EmployeeRepository<Employee>(sessionContext);
-
-            while (index != 3)
-            {
-   
-                // Create button + assign values...
-                var josh = new Employee {FirstName = "Josh " + index};
-
-                try
-                {
-                    // Save button
-                    repo.Save(josh);
-                    repo.Commit();
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Jump");
-                }
-
-                Console.WriteLine("Write " + index);
-                if (index == 0)
-                    Console.WriteLine("Shutdown SQL server now...");
-                Console.ReadLine();
-                index++;
-            }
-
-
-            /*foreach (var employ in repo.GetAll())
-            {
-                Console.WriteLine("Name : " + employ.FirstName);
-            }*/
-
-            Console.WriteLine("Done!");
             Console.ReadKey();
         }
 
+        private static void Sync()
+        {
+            Console.Clear();
+            _sync.SyncAllTables();
+        }
+
+        private static void GetLastRun_Status()
+        {
+            var elLastRun = _configReader.GetAllInstancesOf("LastRun");
+            var status = elLastRun[0].Element("StatusLocal")?.Value;
+
+            bool value;
+            if (bool.TryParse(status, out value))
+            {
+                BaseConfig.IsLocal = value;
+            }
+        }
+
+        private static void SaveRun_Status()
+        {
+            var elLastRun = _configReader.GetAllInstancesOf("LastRun");
+            var status = elLastRun[0].Element("StatusLocal");
+            status?.SetValue(BaseConfig.IsLocal.ToString());
+        }
+
+        #region EventListeners
         private static void Sync_OnCleanUp(object sender, SyncEngine.Events.SyncCleanupArgs e)
         {
             Console.WriteLine("\nCleaning up - " + e.Status);
@@ -92,8 +79,7 @@ namespace RepoConsole
 
         private static void Sync_OnSyncStart(object sender, SyncEngine.Events.SyncStartedArgs e)
         {
-            Console.Clear();
-            Console.WriteLine("\n" + e.Status);
+            Console.WriteLine("\nSyncing... " + e.Status);
         }
 
         private static void Sync_OnUpdateStatus(object sender, SyncEngine.Events.ProgressEventArgs e)
@@ -105,12 +91,14 @@ namespace RepoConsole
         {
             Console.Clear();
             Console.WriteLine("\n" + e.Status);
-            Console.WriteLine("Error: " + e.ExceptionObject.Message);
+            Console.WriteLine("Error: " + (e.ExceptionObject.InnerException?.Message ?? e.ExceptionObject.Message));
+            Console.ReadLine();
         }
 
         private static void Sync_OnSyncComplete(object sender, SyncEngine.Events.SyncCompleteArgs e)
         {
             Console.WriteLine("\n" + e.Status);
         }
+        #endregion
     }
 }
