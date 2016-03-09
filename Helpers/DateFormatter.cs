8debd0d83268ad154e1b818e305b936750ca8b93
Josh Preece <joshpreece@outlook.com>
2016-03-09 18:14:12 +0000

    Added new DateFormatter
    - Removes milliseconds from datetime for access database to work

---
+++ Helpers/DateFormatter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers
+{
+    public static class DateFormatter
+    {
+        /// <summary>
+        /// Truncate the milliseconds from date time this
+        /// is to allow access to work with date time
+        /// FROM: http://stackoverflow.com/questions/16217464/trying-to-insert-datetime-now-into-date-time-field-gives-data-type-mismatch-er
+        /// </summary>
+        /// <param name="d">Date time</param>
+        /// <returns>Formatted date time</returns>
+        public static DateTime GetDateWithoutMilliseconds(DateTime d)
+        {
+            return new DateTime(d.Year, d.Month, d.Day, d.Hour, d.Minute, d.Second);
+        }
+    }
+}
