8a2f95b5d705ecbb9ee5a43a35f98f22a9d1da60
Josh Preece <joshpreece@outlook.com>
2016-03-09 17:13:11 +0000

    Program sync on start and exit logic

--- RepoConsole/Program.cs
+++ RepoConsole/Program.cs
@@ -20,10 +20,12 @@ namespace RepoConsole
         private static SyncManager _sync;
         private static ConfigReader _configReader;
 
+        private const string SettingsPath = @"C:\repoSettings.xml";
+
         static void Main(string[] args)
         {
             // Load settings
-            _configReader = new ConfigReader(@"C:\repoSettings.xml");
+            _configReader = new ConfigReader(SettingsPath);
             BaseConfig.Sources = _configReader.GetAllInstancesOf("ConnectionString");
 
             // Register sync events
@@ -34,16 +36,23 @@ namespace RepoConsole
             _sync.OnSyncStart += Sync_OnSyncStart;
             _sync.OnCleanUp += Sync_OnCleanUp;
 
-            Sync();
-            //BaseConfig.IsLocal = false;
+            if (WasLastRun_Local())
+            {
+                BaseConfig.IsLocal = false;
+                Sync();
+            }
 
             var main = new MainView();
             main.Show();
 
+            SaveRun_Status();
             if (BaseConfig.IsLocal)
+            {
+                BaseConfig.IsLocal = false;
                 Sync();
+            }
 
-            Console.ReadKey();
+            //Console.ReadKey();
         }
 
         private static void Sync()
@@ -52,16 +61,16 @@ namespace RepoConsole
             _sync.SyncAllTables();
         }
 
-        private static void GetLastRun_Status()
+        private static bool WasLastRun_Local()
         {
             var elLastRun = _configReader.GetAllInstancesOf("LastRun");
             var status = elLastRun[0].Element("StatusLocal")?.Value;
 
             bool value;
-            if (bool.TryParse(status, out value))
-            {
-                BaseConfig.IsLocal = value;
-            }
+            if (bool.TryParse(status, out value) == false)
+                throw new Exception("Could not read last run status!");
+
+            return value;
         }
 
         private static void SaveRun_Status()
@@ -69,6 +78,7 @@ namespace RepoConsole
             var elLastRun = _configReader.GetAllInstancesOf("LastRun");
             var status = elLastRun[0].Element("StatusLocal");
             status?.SetValue(BaseConfig.IsLocal.ToString());
+            _configReader.SaveChanges(SettingsPath);
         }
 
         #region EventListeners
@@ -97,6 +107,8 @@ namespace RepoConsole
 
         private static void Sync_OnSyncComplete(object sender, SyncEngine.Events.SyncCompleteArgs e)
         {
+            SaveRun_Status();
+
             Console.WriteLine("\n" + e.Status);
             Console.Write("\nPress any key to continue...");
             Console.ReadKey();
